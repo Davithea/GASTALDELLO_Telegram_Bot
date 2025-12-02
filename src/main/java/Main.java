064fef650da14dc1cd76830a0b6f1dc74c4ba813
GASTALDELLO DAVIDE <10934165@itisrossi.vi.it>
2025-12-02 11:57:26 +0100

    Commit iniziale con aggiunta di alcuni file

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+        MyConfiguration myConfiguration = MyConfiguration.getInstance();
+
+        try {
+            String botToken = myConfiguration.getProperty("BOT_TOKEN");
+            TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+            botsApplication.registerBot(botToken, new GASTALDELLO_Telegram_Bot());
+        } catch(TelegramApiException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
